f40f9344fd4d56a0dd964af6dba86ebaa899d5de
Local MM <73925149+mymy-4242@users.noreply.github.com>
2021-01-16 23:43:48 -0800

    added forecast container showing city and date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 var searchFormEl = document.querySelector("#search-form");
 var cityInputEl = document.querySelector("#city");
 var citiesContainerEl = document.querySelector("#cities-container");
+var weatherContainerEl = document.querySelector("#weather-container");
 
 var getWeather = function(city) {
     //format the weather url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&cnt=5&appid=6ce2fd47036a82a20821ae0d7d209616";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&cnt=6&appid=6ce2fd47036a82a20821ae0d7d209616";
     
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             displayWeather(data, city);
+            console.log(data);
         });
     });
 };
@@ -38,7 +40,29 @@ var displayWeather = function(forecast, searchTerm) {
 
     citySearchedEl.appendChild(cityEl);
     citiesContainerEl.appendChild(citySearchedEl);
-}
+
+    //create a div for the forecast
+    //var forecastEl = forecast.list[0].main.temp + "/" + forecast.list[0].main.humidity + "/" + forecast.list[0].wind.speed;
+
+    //create weather card
+    var forecastContainerEl = document.createElement("card");
+
+    //format weather title
+    var weatherTitle = forecast.city.name + " (" + forecast.list[0].dt_txt + ")";
+
+    //create weather card title
+    var weatherTitleEl = document.createElement("h3");
+    weatherTitleEl.classList = "card-header";
+    weatherTitleEl.textContent = weatherTitle;
+
+
+    forecastContainerEl.appendChild(weatherTitleEl);
+
+
+    //forecastContainerEl.value = forecastEl;
+    weatherContainerEl.appendChild(forecastContainerEl);
+    
+};
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
 
